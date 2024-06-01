3b5ebb7c78a5fa8942cae6a94d3d80cd45840e31
unknown <trane-101@yandex.ru>
2024-06-01 11:32:58 +0400

    fixed a bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,33 @@ st.title("XGBRegressor Model")
 
 st.write("Enter input data:")
 
+input_data = []
+'''
+'fullyVaccinated', 'imputed_total_cases', 'imputed_total_deaths',
+'new_vaccinations_smoothed', 'partiallyVaccinated', 'stringency_index',
+'test24hours', 'totalTests', 'totalVaccinations',
+'total_tests_per_thousand', 'vaccinated24hours', 'rfh', 'r3h', 'day', 'day_of_week', 'month'
+'''
+input_value_1 = st.number_input("Fully Vaccinated", min_value=9327654.0, step=1)
+input_value_2 = st.number_input('Total Deaths', min_value=4072.0, step=1)
+input_value_3 = st.number_input("New Vaccinations", min_value=0, step=1)
+input_value_4 = st.number_input("Partially Vaccinated", min_value=4663827.0, step=1)
+input_value_5 = st.number_input("Stringency Index", min_value=0)
+input_value_6 = st.number_input("Tests in the last 24 hours", min_value=0)
+input_value_7 = st.number_input("Total Tests", min_value=4166833.0, step=1)
+input_value_8 = st.number_input("Total Vaccinations", min_value=9982068.0, step=1)
+input_value_9 = st.number_input("Total Tests per 1000", min_value=0)
+input_value_10 = st.number_input("Vaccinated in the last 24 hours", min_value=0)
+input_value_11 = st.number_input("rfh", min_value=0)
+input_value_12 = st.number_input("r3h", min_value=0)
+input_value_13 = st.number_input("Day (out of 365)", min_value=1, max_value=365, step=1)
+input_value_14 = st.number_input("Day of the week", min_value=0, max_value=6, step=1)
+input_value_15 = st.number_input("Month", min_value=1, max_value=12, step=1)
+
+input_data.append(input_value_1, input_value_2, input_value_3, input_value_4, input_value_5, input_value_6, input_value_7,
+                  input_value_8, input_value_9, input_value_10, input_value_11, input_value_12, input_value_13, input_value_14,
+                  input_value_15)
+
+if st.button("Predict"):
+    prediction = predict(input_data)
+    st.write(f"Prediction: {prediction}")
