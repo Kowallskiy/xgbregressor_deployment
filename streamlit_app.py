2358b2e60db8badabc343965a97a548f8e678c41
unknown <trane-101@yandex.ru>
2024-06-02 12:48:43 +0400

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,12 @@ def predict(input_data):
 
 st.title("XGBRegressor Model")
 
+st.write("The model predicts total confirmed COVID-19 cases in Zambia")
+
 st.write("Enter input data:")
 
 input_data = []
-'''
-'fullyVaccinated', 'imputed_total_cases', 'imputed_total_deaths',
-'new_vaccinations_smoothed', 'partiallyVaccinated', 'stringency_index',
-'test24hours', 'totalTests', 'totalVaccinations',
-'total_tests_per_thousand', 'vaccinated24hours', 'rfh', 'r3h', 'day', 'day_of_week', 'month'
-'''
+
 input_value_1 = st.number_input("Fully Vaccinated", min_value=9327654, step=1)
 input_value_2 = st.number_input('Total Deaths', min_value=4072, step=1)
 input_value_3 = st.number_input("New Vaccinations", min_value=0, step=1)
@@ -38,9 +35,6 @@ input_value_15 = st.number_input("Month", min_value=1, max_value=12, step=1)
 input_data.extend([input_value_1, input_value_2, input_value_3, input_value_4, input_value_5, input_value_6, input_value_7,
                   input_value_8, input_value_9, input_value_10, input_value_11, input_value_12, input_value_13, input_value_14,
                   input_value_15])
-
-st.write(f"Input Data: {input_data}")
-st.write(f"Input Data Shape: {np.array(input_data).reshape(1, -1).shape}")
 
 if st.button("Predict"):
     prediction = predict(input_data)
